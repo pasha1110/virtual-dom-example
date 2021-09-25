04ab92af1884f5eb333f664db7bcefcaaa1f659b
pasha1110 <perlangga552@gmail.com>
2021-09-25 12:06:24 +0700

    1 file modified

--- App.js
+++ App.js
@@ -1,16 +1,8 @@
 import { h } from './vnode.js';
 
-function Button(text, color = '#1abc9c') {
-  return h('button', {}, text);
-}
-
-function Div(children) {
-  return h('div', null, children);
-}
-
 // component
 function App() {
-  return Button('Button');
+  return h("h1",{},"Hello world");
 }
 
 export default App;
