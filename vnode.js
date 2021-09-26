f60f175503a812e66f3b83aa76363277fdc887cd
pasha1110 <perlangga552@gmail.com>
2021-09-26 12:27:24 +0700

    1 file modified

--- vnode.js
+++ vnode.js
@@ -28,4 +28,37 @@ function unmount(vnode) {
   vnode.el.parentNode.removeChild(vnode.el);
 }
 
-export { h, mount, unmount };
+var activeEffect;
+
+function watchEffect(fn) {
+  activeEffect = fn;
+  fn();
+  activeEffect = null;
+}
+
+class Dep {
+  constructor(value) {
+    this._value = value;
+    this.subscribers = new Set();
+  }
+
+  depend() {
+    if (activeEffect) this.subscribers.add(activeEffect);
+  }
+
+  notify() {
+    this.subscribers.forEach((subscriber) => subscriber());
+  }
+
+  get value() {
+    this.depend();
+    return this._value;
+  }
+
+  set value(newValue) {
+    this._value = newValue;
+    this.notify();
+  }
+}
+
+export { h, mount, unmount, watchEffect};
