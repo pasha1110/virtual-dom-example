934cb061204d214c99f29fba1bd5617da55ff8d4
pasha1110 <perlangga552@gmail.com>
2021-10-04 19:36:07 +0700

    1 file modified, 4 added, 4 deleted

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-import { mount } from './vnode.js';
-import { App, Home } from './App.js';
-import Router from './vnode--router.js';
+import { mount } from './lib/vnode.js';
+import { App } from './src/App.js';
 
-const route = new Router();
-route.get('/App', function () {
-  mount(document.getElementById('app'), App());
-});
-mount (document.getElementById("app"),Home())
-route.init();
+mount(document.querySelector('#app'), App());
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import { h } from '../lib/vnode.js';
+// component
+function App() {
+  return h('h1', null, `hello world`);
+}
+
+export { App };
